1d4d663da138a986e6ddd1f0890593b100d489db
sfistak <grzegorz.smajdor@gmail.com>
2021-11-16 21:44:39 +0100

    clojure day 1

---
+++ clj/src/clj/day1.clj
@@ -0,0 +1,30 @@
+(ns clj.day1)
+
+(defn read-file []
+  (slurp "resources/day1-input.txt"))
+
+(defn find-matching-numbers-1 []
+  (def numbers (clojure.string/split-lines (read-file)))
+  (for [x numbers
+        y numbers
+        z numbers
+        :when (= (+ (Integer/parseInt x) (Integer/parseInt y)) 2020)]
+      (flatten [(Integer/parseInt x) (Integer/parseInt y)])))
+
+(defn find-matching-numbers-2 []
+  (def numbers (clojure.string/split-lines (read-file)))
+  (for [x numbers
+        y numbers
+        z numbers
+        :when (= (+ (Integer/parseInt x) 
+                    (Integer/parseInt y)
+                    (Integer/parseInt z)) 2020)]
+      (flatten [(Integer/parseInt x) (Integer/parseInt y) (Integer/parseInt z)])))
+
+
+(defn calculate-1 []
+  (reduce * (distinct (flatten (distinct (find-matching-numbers-1)))))
+
+(defn calculate-2 []
+  (reduce * (distinct (flatten (distinct (find-matching-numbers-2)))))
+
