bc14cf1f2880e7e3c734b26f0c8a46e3a9efbad7
sfistak <grzegorz.smajdor@gmail.com>
2021-11-16 20:01:55 +0100

    day 1

---
+++ ruby/day1/day1.rb
@@ -0,0 +1,26 @@
+require 'byebug'
+
+def read_file
+  file = File.open("input.txt")
+  file.readlines.map(&:chomp)
+end
+
+def find_numbers_that_sums_to_2020
+  numbers = read_file
+  matched_numbers = []
+  numbers.each do |number|
+    numbers.each do |number2|
+      numbers.each do |number3|
+        if number.to_i + number2.to_i + number3.to_i == 2020
+          matched_numbers << [number.to_i, number2.to_i]
+        end
+      end
+    end
+  end
+  matched_numbers.flatten.uniq!
+end
+
+numbers = find_numbers_that_sums_to_2020.uniq
+puts numbers
+sum = numbers[0].to_i * numbers[1].to_i * numbers[2].to_i
+puts "sum #{sum}"
