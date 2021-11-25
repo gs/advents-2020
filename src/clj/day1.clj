00ee2796112165de019a769b595cce89eb9f7964
sfistak <grzegorz.smajdor@gmail.com>
2021-11-25 22:01:25 +0100

    Refactor of day 1

--- src/clj/day1.clj
+++ src/clj/day1.clj
@@ -1,32 +1,26 @@
 ;; https://adventofcode.com/2020/day/1
 (ns clj.day1)
 
-(defn read-file []
-  (slurp "resources/day1-input.txt"))
+(def read-file 
+  (map #(Integer/parseInt %)
+       (clojure.string/split-lines
+        (slurp "resources/day1-input.txt"))))
+
 
 (defn find-matching-numbers-1 []
-  (def numbers (clojure.string/split-lines (read-file)))
-  (for [x numbers
-        y numbers
-        :when (= (+ (Integer/parseInt x) (Integer/parseInt y)) 2020)]
-    (flatten [(Integer/parseInt x) (Integer/parseInt y)])))
+  (first (for [x read-file
+               y read-file
+               :when (= (+ x y) 2020)]
+           (* x y))))
 
-
-
+(find-matching-numbers-1)
 
 (defn find-matching-numbers-2 []
-  (def numbers (clojure.string/split-lines (read-file)))
-  (for [x numbers
-        y numbers
-        z numbers
-        :when (= (+ (Integer/parseInt x)
-                    (Integer/parseInt y)
-                    (Integer/parseInt z)) 2020)]
-    (flatten [(Integer/parseInt x) (Integer/parseInt y) (Integer/parseInt z)])))
+  (first (for [x read-file
+               y read-file
+               z read-file
+               :when (= (+ x y z) 2020)]
+           (* x y z ))))
 
 
-(defn calculate-1 []
-  (reduce * (distinct (flatten (distinct (find-matching-numbers-1))))))
-
-(defn calculate-2 []
-  (reduce * (distinct (flatten (distinct (find-matching-numbers-2))))))
+(find-matching-numbers-2)
